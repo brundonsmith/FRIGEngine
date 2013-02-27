cd99967c5ce4aff003212f5994e584a2502ba5a9
unknown <reagan_thomas@baylor.edu>
2013-02-27 17:02:58 -0600

    test restructuring

--- src/frigengine/tests/ExceptionsTest.java
+++ src/frigengine/tests/ExceptionsTest.java
@@ -3,6 +3,13 @@ package frigengine.tests;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
+import frigengine.exceptions.tests.AttributeFormatExceptionTest;
+import frigengine.exceptions.tests.AttributeMissingExceptionTest;
+import frigengine.exceptions.tests.CommandExceptionTest;
+import frigengine.exceptions.tests.ComponentExceptionTest;
+import frigengine.exceptions.tests.DataParseExceptionTest;
+import frigengine.exceptions.tests.InvalidTagExceptionTest;
+
 @RunWith(Suite.class)
 @Suite.SuiteClasses({ AttributeFormatExceptionTest.class,
 		AttributeMissingExceptionTest.class, CommandExceptionTest.class,
